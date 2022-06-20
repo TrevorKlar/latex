673e2318a4736fc5ade36903db4ca1d32a26dbec
Trevor Klar <trevorsklar@gmail.com>
2022-06-20 16:18:17 -0700

    Completed the replacement

--- 34a/_Summer 2022/Lectures/sed-commands.sh
+++ 34a/_Summer 2022/Lectures/sed-commands.sh
@@ -5,9 +5,10 @@ cd /Users/trevor/git/latex/34a/_Summer\ 2022
 for f in Lectures/**/*.tex
 
 do
-sed -i 's/Peter M.\\ Garfield/Trevor Klar/g' "$f"
-sed -i 's/garfield@math.ucsb.edu/trevorklar@math.ucsb.edu/g' "$f"
-sed -i 's/South Hall 6510/South Hall 6431X (Grad Tower, 6th floor, blue side, first door on the right)/g' "$f"
-sed -i 's/(Office Hours:}\\\\\n.*?})(.*\n.*\n.*\n.*\n)/$1 MTWR after class 2:00-3:00, and by appointment (Really! Just send me an email and I’m happy to make an appointment). I’m also available for appointments by Zoom./' "$f"
+#echo "$f"
+sed -i .bak 's/Peter M.\\ Garfield/Trevor Klar/g' "$f"
+sed -i .bak 's/garfield@math.ucsb.edu/trevorklar@math.ucsb.edu/g' "$f"
+sed -i .bak 's/South Hall 6510/South Hall 6431X (Grad Tower, 6th floor, blue side, first door on the right)/g' "$f"
+sed -i .bak 's/(Office Hours:}\\\\\n.*?})(.*\n.*\n.*\n.*\n)/$1 MTWR after class 2:00-3:00, and by appointment (Really! Just send me an email and I’m happy to make an appointment). I’m also available for appointments by Zoom./' "$f"
 
 done
