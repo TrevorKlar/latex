74e0784c8ffc08f9672e834220f5b0398ca55c35
Trevor Klar <trevorsklar@gmail.com>
2022-06-20 17:18:23 -0700

    Imported Nathan's Lectures

--- 34a/_Summer 2022/Lectures/compile-all.sh
+++ 34a/_Summer 2022/Lectures/compile-all.sh
@@ -2,12 +2,4 @@
 
 cd /Users/trevor/git/latex/34a/_Summer\ 2022
 
-for f in Lectures/**/*.tex
-
-do
-#echo "$f"
-cd $(dirname "$f")
-/Library/TeX/texbin/pdflatex -interaction=nonstopmode $(basename "$f")
-cd ../
-
-done
+for f in Lectures/**/*.tex; do cd $(dirname "$f") && /Library/TeX/texbin/pdflatex -interaction=nonstopmode $(basename "$f") && /Library/TeX/texbin/pdflatex -interaction=nonstopmode $(basename "$f") && cd ../; done
